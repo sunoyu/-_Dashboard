009627ef885743d68161ddcbf8778922a0ff002e
sunoyu <suno_yu@naver.com>
2024-08-25 23:36:46 +0900

    네비게이션바

---
+++ src/components/atoms/buttons/IconButton.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="icon-button">
+    <img
+      :src="`src/assets/icons/${icon}.svg`"
+      alt=""
+      class="icon-button__image"
+    />
+    <img
+      :src="`src/assets/icons/${icon}-hover.svg`"
+      alt=""
+      class="icon-button__image-hover"
+    />
+    <span class="icon-button__label">{{ label }}</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { toRefs } from "vue";
+interface Props {
+  icon: string;
+  label: string;
+}
+
+const props = defineProps<Props>();
+const { icon, label } = toRefs(props);
+</script>
+
+<style scoped>
+.icon-button {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+
+  width: calc(100% -16px);
+
+  padding: 12px 18px;
+  gap: 16px;
+
+  &__image-hover {
+    display: none;
+  }
+  &__label {
+    font-weight: 500;
+    font-size: 19px;
+
+    color: $color-gray-500;
+  }
+  &:hover {
+    background-color: $color-gray-100;
+    border-radius: 4px;
+
+    .icon-button__label {
+      color: $color-gray-900;
+    }
+    .icon-button__image {
+      display: none;
+    }
+    .icon-button__image-hover {
+      display: block;
+    }
+  }
+}
+</style>
--- src/components/organisms/common/Navigation.vue
+++ src/components/organisms/common/Navigation.vue
@@ -1,7 +1,22 @@
 <template>
-  <div>네비게이션</div>
+  <div class="navigation">
+    <img src="scr/assets/images/logo.png" alt="" class="navigation__logo" />
+    <div class="navigation__menu-box">
+      <div class="navigation__menu-box__top">
+        <RouterLink to="/" class="menu">
+          <IconButton :icon="'folder'" :label="'내 포트폴리오'"></IconButton>
+        </RouterLink>
+        <RouterLink to="/about" class="menu">
+          <IconButton :icon="'chart'" :label="'종목 검색기'"></IconButton>
+        </RouterLink>
+      </div>
+      <div class="navigation__menu-box__bottom"></div>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import IconButton from "@/components/atoms/buttons/IconButton.vue";
+</script>
 
 <style scoped></style>
