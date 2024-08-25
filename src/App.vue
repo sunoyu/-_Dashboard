d8cfd4a9871639a007913fcbe004f52722a68ac3
sunoyu <suno_yu@naver.com>
2024-08-25 18:00:31 +0900

    first

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>App.vue</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { fileURLToPath, URL } from "url";
+//npm install @types/node 로 오류 해결
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "@assets": fileURLToPath(new URL("./src/assets", import.meta.url)),
+      "@components": fileURLToPath(
+        new URL("./src/components", import.meta.url)
+      ),
+      "@pages": fileURLToPath(new URL("./src/pages", import.meta.url)),
+      "@routes": fileURLToPath(new URL("./src/routes", import.meta.url)),
+      "@stores": fileURLToPath(new URL("./src/stores", import.meta.url)),
+      "@apis": fileURLToPath(new URL("./src/apis", import.meta.url)),
+    },
+  },
+  // SCSS 전역 사용
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@import "./src/assets/styles/main.scss',
+      },
+    },
+  },
+});
